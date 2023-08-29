1fb7b1672c9640c4ae2d38a7be5db4ddbc0c914f
Alejandro Gonzalez Romero <me@algorodev.com>
2023-08-29 18:04:39 +0200

    feat: validations

--- src/calculator.js
+++ src/calculator.js
@@ -82,6 +82,7 @@ element.divisionButton.addEventListener('click', () => {
 
 element.cleanButton.addEventListener('click', () => {
 	clearCalculator()
+	updateScreenValue()
 })
 
 element.backButton.addEventListener('click', () => {
@@ -99,12 +100,21 @@ export const onNumberClick = (selectedNumber) => {
 }
 
 export const onOperationClick = (selectedOperation) => {
+	if (screenValue === '') return
+
 	operation = selectedOperation
 	firstNumber = screenValue
 	screenValue = ''
+
+	element.additionButton.classList.add('calculator__control--disabled')
+	element.subtractionButton.classList.add('calculator__control--disabled')
+	element.multiplicationButton.classList.add('calculator__control--disabled')
+	element.divisionButton.classList.add('calculator__control--disabled')
 }
 
 export const onBackClick = () => {
+	if (screenValue === '') return
+
 	screenValue = screenValue.substring(0, screenValue.length - 1)
 }
 
@@ -113,16 +123,36 @@ export const updateScreenValue = () => {
 }
 
 export const clearScreenValue = () => {
-	element.screen.textContent = null
+	element.screen.textContent = ''
 }
 
 export const clearCalculator = () => {
 	screenValue = ''
 	firstNumber = ''
 	operation = ''
+
+	element.nineButton.classList.remove('calculator__control--disabled')
+	element.eightButton.classList.remove('calculator__control--disabled')
+	element.sevenButton.classList.remove('calculator__control--disabled')
+	element.sixButton.classList.remove('calculator__control--disabled')
+	element.fiveButton.classList.remove('calculator__control--disabled')
+	element.fourButton.classList.remove('calculator__control--disabled')
+	element.threeButton.classList.remove('calculator__control--disabled')
+	element.twoButton.classList.remove('calculator__control--disabled')
+	element.oneButton.classList.remove('calculator__control--disabled')
+	element.zeroButton.classList.remove('calculator__control--disabled')
+	element.commaButton.classList.remove('calculator__control--disabled')
+	element.additionButton.classList.remove('calculator__control--disabled')
+	element.subtractionButton.classList.remove('calculator__control--disabled')
+	element.multiplicationButton.classList.remove('calculator__control--disabled')
+	element.divisionButton.classList.remove('calculator__control--disabled')
+	element.backButton.classList.remove('calculator__control--disabled')
+	element.resultButton.classList.remove('calculator__control--disabled')
 }
 
 export const calculateResult = () => {
+	if (firstNumber === '' || operation === '') return
+
 	switch (operation) {
 		case '+':
 			screenValue = addition(+firstNumber, +screenValue) + ''
@@ -137,4 +167,18 @@ export const calculateResult = () => {
 			screenValue = division(+firstNumber, +screenValue) + ''
 			break
 	}
+
+	element.nineButton.classList.add('calculator__control--disabled')
+	element.eightButton.classList.add('calculator__control--disabled')
+	element.sevenButton.classList.add('calculator__control--disabled')
+	element.sixButton.classList.add('calculator__control--disabled')
+	element.fiveButton.classList.add('calculator__control--disabled')
+	element.fourButton.classList.add('calculator__control--disabled')
+	element.threeButton.classList.add('calculator__control--disabled')
+	element.twoButton.classList.add('calculator__control--disabled')
+	element.oneButton.classList.add('calculator__control--disabled')
+	element.zeroButton.classList.add('calculator__control--disabled')
+	element.commaButton.classList.add('calculator__control--disabled')
+	element.backButton.classList.add('calculator__control--disabled')
+	element.resultButton.classList.add('calculator__control--disabled')
 }
